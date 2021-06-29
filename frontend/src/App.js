c9e3c14b3f653b8511fb458e5d6849c25a07f25c
MMHO <MMHO@itu.dk>
2021-06-29 14:07:28 +0200

    Previous 2 commits fixed the pipeline to be runnable on the RPI arm chip. This commit changes the frontend to see it in effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello world!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactsss
-        </a>
       </header>
     </div>
   );
